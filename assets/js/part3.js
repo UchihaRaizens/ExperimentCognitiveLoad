b0d4f1fbc353e86a1c1ea73656d5f4687170b7b2
Lukáš Rešutík <lresutik@gmail.com>
2017-03-26 00:11:07 +0100

    add data set 2
    rework second part

--- assets/js/part3.js
+++ assets/js/part3.js
@@ -5,62 +5,62 @@ var mathTest = [
       {
         id:1,
         math_example: '1 + 1 + 0 + 1 + 1 = ?',
-        math_options:[1,2,3,4],
+        math_options:[1,2,3,4,5],
         correct: 4
       },
        {
         id:2,
         math_example: '0 + 1 + 1 + 1 + 0 = ?',
-        math_options:[4,2,3,1],
+        math_options:[4,2,5,3,1],
         correct: 3
       },
        {
         id:3,
         math_example: '1 + 0 + 1 + 0 + 1 = ?',
-        math_options:[2,1,4,3],
+        math_options:[5,2,1,4,3],
         correct: 3
       },
        {
         id:4,
-        math_example: '1 + 0 + 1 + 0 + 1 = ?',
-        math_options:[4,1,4,3],
-        correct: 3
+        math_example: '1 + 0 + 0 + 0 + 1 = ?',
+        math_options:[2,1,4,3,5],
+        correct: 2
       },
        {
         id:5,
-        math_example: '1 + 0 + 1 + 0 + 1 = ?',
-        math_options:[5,1,4,3],
-        correct: 3
+        math_example: '0 + 0 + 1 + 0 + 1 = ?',
+        math_options:[5,1,4,3,2],
+        correct: 2
       },
        {
         id:6,
-        math_example: '1 + 0 + 1 + 0 + 1 = ?',
-        math_options:[6,1,4,3],
-        correct: 3
+        math_example: '1 + 1 + 1 + 1 + 1 = ?',
+        math_options:[0,1,5,4,3],
+        correct: 5
       },
        {
         id:7,
-        math_example: '1 + 0 + 1 + 0 + 1 = ?',
-        math_options:[7,1,4,3],
-        correct: 3
+        math_example: '1 + 1 + 1 + 0 + 1 = ?',
+        math_options:[0,1,4,3,5],
+        correct: 4
       },
        {
         id:8,
-        math_example: '1 + 0 + 1 + 0 + 1 = ?',
-        math_options:[8,1,4,3],
+        math_example: '1 + 1 + 1 + 0 + 0 = ?',
+        math_options:[2,1,5,4,3],
         correct: 3
       },
        {
         id:9,
-        math_example: '1 + 0 + 1 + 0 + 1 = ?',
-        math_options:[9,1,4,3],
+        math_example: '0 + 0 + 0 + 1 + 1 = ?',
+        math_options:[5,1,4,3,2],
         correct: 3
       },
        {
         id:10,
-        math_example: 'pos?',
-        math_options:[10,1,4,3],
-        correct: 3
+        math_example: '0 + 0 + 1 + 0 + 0 = ?',
+        math_options:[0,1,2,4,3],
+        correct: 1
       }
     ]
   },
@@ -70,62 +70,62 @@ var mathTest = [
       {
         id:1,
         math_example: '1 + 2 + 3 + 4 + 5 = ?',
-        math_options:[1,2,3,4],
-        correct: 4
+        math_options:[10,12,13,15,17],
+        correct: 15
       },
        {
         id:2,
-        math_example: '1 + 2 + 3 + 4 + 5 = ?',
-        math_options:[4,2,3,1],
-        correct: 3
+        math_example: '5 + 3 + 3 + 2 + 1 = ?',
+        math_options:[14,12,13,15,16],
+        correct: 14
       },
        {
         id:3,
-        math_example: '1 + 2 + 3 + 4 + 5 = ?',
-        math_options:[2,1,4,3],
-        correct: 3
+        math_example: '4 + 3 + 4 + 2 + 5 = ?',
+        math_options:[20,19,15,17,18],
+        correct: 18
       },
        {
         id:4,
-        math_example: '1 + 2 + 3 + 4 + 5 = ?',
-        math_options:[3,1,4,3],
-        correct: 3
+        math_example: '3 + 1 + 5 + 4 + 5 = ?',
+        math_options:[17,16,18,19,15],
+        correct: 18
       },
        {
         id:5,
-        math_example: '1 + 2 + 3 + 4 + 5 = ?',
-        math_options:[4,1,4,3],
-        correct: 3
+        math_example: '2 + 2 + 5 + 4 + 1 = ?',
+        math_options:[14,15,16,17,12],
+        correct: 14
       },
        {
         id:6,
-        math_example: '1 + 2 + 3 + 4 + 5 = ?',
-        math_options:[5,1,4,3],
-        correct: 3
+        math_example: '4 + 3 + 5 + 4 + 5 = ?',
+        math_options:[20,21,19,18,22,17],
+        correct: 21
       },
        {
         id:7,
-        math_example: '1 + 2 + 3 + 4 + 5 = ?',
-        math_options:[6,1,4,3],
-        correct: 3
+        math_example: '3 + 3 + 3 + 4 + 4 = ?',
+        math_options:[16,20,19,18,17],
+        correct: 17
       },
        {
         id:8,
-        math_example: '1 + 2 + 3 + 4 + 5 = ?',
-        math_options:[8,1,4,3],
-        correct: 3
+        math_example: '1 + 5 + 4 + 2 + 5 = ?',
+        math_options:[13,17,14,15,18],
+        correct: 17
       },
        {
         id:9,
-        math_example: '1 + 2 + 3 + 4 + 5 = ?',
-        math_options:[9,1,4,3],
-        correct: 3
+        math_example: '5 + 5 + 5 + 4 + 5 = ?',
+        math_options:[22,20,24,23,25],
+        correct: 24
       },
        {
         id:10,
-        math_example: '1 + 2 + 3 + 4 + 5 = ?',
-        math_options:[10,1,4,3],
-        correct: 3
+        math_example: '2 + 2 + 3 + 3 + 1 = ?',
+        math_options:[10,11,14,15,16],
+        correct: 11
       }
     ]
   },
@@ -135,62 +135,62 @@ var mathTest = [
       {
         id:1,
         math_example: '1 + 3 + 5 + 8 + 10 = ?',
-        math_options:[1,2,3,4],
-        correct: 4
+        math_options:[18,27,29,22,25],
+        correct: 27
       },
        {
         id:2,
-        math_example: '1 + 3 + 5 + 8 + 10 = ?',
-        math_options:[4,2,3,1],
-        correct: 3
+        math_example: '8 + 7 + 9 + 2 + 10 = ?',
+        math_options:[37,38,36,34,39],
+        correct: 36
       },
        {
         id:3,
-        math_example: '1 + 3 + 5 + 8 + 10 = ?',
-        math_options:[2,1,4,3],
-        correct: 3
+        math_example: '10 + 3 + 5 + 6 + 10 = ?',
+        math_options:[30,31,29,34,37],
+        correct: 34
       },
        {
         id:4,
-        math_example: '1 + 3 + 5 + 8 + 10 = ?',
-        math_options:[4,1,4,3],
-        correct: 3
+        math_example: '6 + 7 + 3 + 8 + 2 = ?',
+        math_options:[28,30,29,27,26],
+        correct: 26
       },
        {
         id:5,
-        math_example: '1 + 3 + 5 + 8 + 10 = ?',
-        math_options:[5,1,4,3],
-        correct: 3
+        math_example: '4 + 9 + 5 + 8 + 7 = ?',
+        math_options:[38,36,37,33,32],
+        correct: 33
       },
        {
         id:6,
-        math_example: '1 + 3 + 5 + 8 + 10 = ?',
-        math_options:[6,1,4,3],
-        correct: 3
+        math_example: '9 + 9 + 2 + 8 + 10 = ?',
+        math_options:[40,38,39,32,35],
+        correct: 38
       },
        {
         id:7,
-        math_example: '1 + 3 + 5 + 8 + 10 = ?',
-        math_options:[7,1,4,3],
-        correct: 3
+        math_example: '2 + 7 + 4 + 5 + 10 = ?',
+        math_options:[30,29,24,23,28],
+        correct: 28
       },
        {
         id:8,
-        math_example: '1 + 3 + 5 + 8 + 10 = ?',
-        math_options:[8,1,4,3],
-        correct: 3
+        math_example: '9 + 10 + 9 + 8 + 10 = ?',
+        math_options:[42,44,46,48,47],
+        correct: 46
       },
        {
         id:9,
-        math_example: '1 + 3 + 5 + 8 + 10 = ?',
-        math_options:[9,1,4,3],
-        correct: 3
+        math_example: '1 + 2 + 8 + 9 + 10 = ?',
+        math_options:[31,28,29,30,27],
+        correct: 30
       },
        {
         id:10,
-        math_example: '1 + 3 + 5 + 8 + 10 = ?',
-        math_options:[10,1,4,3],
-        correct: 3
+        math_example: '5 + 7 + 2 + 9 + 8 = ?',
+        math_options:[31,30,32,34,36],
+        correct: 31
       }
     ]
   },
@@ -357,41 +357,50 @@ var isReady = 0;
 var colorLevel = 0;
 var iteration = 0;
 var isAnswer = 0;
+var isResult = 1;
 
 function runSecondPart() {
-  //console.log('startSecondPart');
   //img = randomize(images)[0];
   //document.body.style.backgroundImage = "url('screens/" + img + "')";
 
-  var word = mathTest[level];
-  var color = colorTest[iteration];
+    var word = mathTest[level];
+    var color = colorTest[iteration];
+    document.getElementById('math_example').innerHTML = word.data[example].math_example;
+    document.getElementById('instruction_2').style.display = "none";
+    document.getElementById('options_reaction_time_task').style.display = "none";
+    document.getElementById(('math_option_0')).innerHTML = word.data[example].math_options[0];
+    document.getElementById(('math_option_1')).innerHTML = word.data[example].math_options[1];
+    document.getElementById(('math_option_2')).innerHTML = word.data[example].math_options[2];
+    document.getElementById(('math_option_3')).innerHTML = word.data[example].math_options[3];
+    document.getElementById(('math_option_4')).innerHTML = word.data[example].math_options[4];
+    document.getElementById('math_options').style.display = "block";
+    
+    if(isAnswer == 0) {
+      isAnswer = 1;
+      isResult = 0;
+      sendEvent('math_example_level_' + word.level.toString() + '_id_' + word.data[example].id.toString());
+    }
 
-  document.getElementById('math_example').innerHTML = word.data[example].math_example;
-  document.getElementById('instruction_2').style.display = "none";
-  document.getElementById('options_reaction_time_task').style.display = "none";
-  document.getElementById(('math_option_0')).innerHTML = word.data[example].math_options[0];
-  document.getElementById(('math_option_1')).innerHTML = word.data[example].math_options[1];
-  document.getElementById(('math_option_2')).innerHTML = word.data[example].math_options[2];
-  document.getElementById(('math_option_3')).innerHTML = word.data[example].math_options[3];
-  document.getElementById('math_options').style.display = "block";
-  if(isAnswer == 0) {
-    isAnswer = 1;
-    sendEvent('math_example_level_' + word.level.toString() + '_id_' + word.data[example].id.toString());
-  }
 
-  if(isReady != 1) {
-    isReady = 1;
-    setTimeout(function() {
-         document.getElementById('math_options').style.display = "none";
-         document.getElementById('instruction_2').style.display = "block";
-         document.getElementById('options_reaction_time_task').style.display = "";
+   // console.log(isReady +  " " + isResult);
+    if(isReady != 1 && isResult == 0) {
+      isReady = 1;
+      //console.log('mozem');
+      setTimeout(function() {
+           isResult = 1;
+           document.getElementById('math_options').style.display = "none";
+           document.getElementById('instruction_2').style.display = "block";
+           document.getElementById('options_reaction_time_task').style.display = "";
 
-         document.getElementById('math_example').innerHTML = color.name_color[colorLevel];
-         document.getElementById('math_example').style.display = "";
-         document.getElementById('math_example').style.color = color.font_color[colorLevel];
-         startReactionTime = new Date().getTime();
-    },3000);
-  }
+           document.getElementById('math_example').innerHTML = color.name_color[colorLevel];
+           document.getElementById('math_example').style.display = "";
+           document.getElementById('math_example').style.color = color.font_color[colorLevel];
+           startReactionTime = new Date().getTime();
+      },5000);
+
+      
+    }
+  
 }
 
 function answerReaction(id) {
@@ -407,11 +416,11 @@ function answerReaction(id) {
 
  
   if(colorLevel == 5) {
-    isAnswer = 0;
-    level += 1;
+  //  isAnswer = 0;
+     // level += 1;
     iteration += 1;
-    colorLevel = 0;
-    example = 0;
+   // colorLevel = 0;
+   // example = 0;
   }
   if(iteration != 5) {
     runSecondPart();
@@ -421,14 +430,28 @@ function answerReaction(id) {
   }
 }
 
+function test() {
+
+}
+
 function answerMath(id) {
   isAnswer = 0;
+  isResult = 0;
   if (mathTest[level].data[example].correct == mathTest[level].data[example].math_options[id]) {
     sendEvent('OK');
   } else {
     sendEvent('WRONG');
   }
+
   example += 1;
+  if(colorLevel == 5) {
+    isAnswer = 0;
+    level += 1;
+  //  iteration += 1;
+    colorLevel = 0;
+    example = 0;
+  }
+
   if(example == 10) {
    example = 0;
   } 
